7c5928b4fa8eef38048860d24edac5fa27a64cf4
Russell Gordon <rgordon@lcs.on.ca>
2025-04-11 14:37:52 -0400

    Initial Commit

---
+++ BoneAppetit/BoneAppetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneAppetitApp.swift
+//  BoneAppetit
+//
+//  Created by Russell Gordon on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct BoneAppetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
